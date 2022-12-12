d7bb1eeefd5f69d8aff64cd4518a3513a28c61be
Babina Banjara <offers682@gmail.com>
2022-12-12 21:39:40 +0545

    added django

---
+++ guideassignment/home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    # return HttpResponse("this is homepage")
+
+# def about(request):
+#     return HttpResponse("this is aboutpage")
+
+# def gallery(request):
+#     return render(request, 'gallery.html')
